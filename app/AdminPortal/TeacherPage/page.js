131b3e7ee96885eee9f13ecdb51d0ce4b6519320
Nabby <trishasalubreaquino@gmail.com>
2025-01-16 12:31:35 -0700

    commit

---
+++ app/AdminPortal/TeacherPage/page.js
@@ -0,0 +1,11 @@
+"use client";
+
+import AdminHeader from "../components/page";
+
+export default function TeacherPage() {
+  return (
+    <section>
+      <div className=""></div>
+    </section>
+  );
+}
