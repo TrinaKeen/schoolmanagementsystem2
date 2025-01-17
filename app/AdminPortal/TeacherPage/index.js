9430eca0174efb3d06717c54d34ea210a5f1d95f
Nabby <trishasalubreaquino@gmail.com>
2025-01-17 15:38:47 -0700

    commit

---
+++ app/AdminPortal/TeacherPage/index.js
@@ -0,0 +1,16 @@
+export const TEACHERS = [
+  {
+    Grade11: ["Mr. Mike Lit", "Mr. Moe Lester", "Mrs. Liz Anya"],
+    Grade12: [
+      "Mr. Hugh Jass",
+      "Ms. Crystal Shandy Leer",
+      "Mrs. Taylor Marie Joy Batumbakal",
+    ],
+    College: {
+      "1st Year": ["Dr. Ben Dover", "Prof. Anita Mann"],
+      "2nd Year": ["Dr. Bud Light", "Prof. Minnie Driver"],
+      "3rd Year": ["Dr. Sid Bubblegum", "Prof. Ray Pits"],
+      "4th Year": ["Dr. Peter File", "Prof. Pat Fenis"],
+    },
+  },
+];
