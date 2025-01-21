4917ce00b506174ff1d7178e84ac98e7dccf5abd
Martin <martin.maquilan@edu.sait.ca>
2025-01-21 08:46:50 -0700

    Created main course pages layout

---
+++ app/AdminPortal/StudentCourses/components/Sidebar.js
@@ -0,0 +1,34 @@
+import Link from 'next/link';
+import styles from './component.module.css';
+
+const Sidebar = () => {
+  return (
+    <div className={styles.sidebar}>
+      <h2>Student Courses</h2>
+      <ul>
+        <li>
+          <Link href="/AdminPortal/StudentCourses/grade11">
+            Senior High School Grade 11
+          </Link>
+        </li>
+        <li>
+          <Link href="/AdminPortal/StudentCourses/grade12">
+            Senior High School Grade 12
+          </Link>
+        </li>
+        <li>
+          <Link href="/AdminPortal/StudentCourses/Midwifery">
+            Bachelor of Science in Midwifery
+          </Link>
+        </li>
+        <li>
+          <Link href="/AdminPortal/StudentCourses/Vocational">
+            Bachelor of Technical-Vocational Teacher Education
+          </Link>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Sidebar;
