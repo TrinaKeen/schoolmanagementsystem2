1cb29ee1fe3c98a56335fb2315325bf5f76f1ce4
Martin <martin.maquilan@edu.sait.ca>
2025-01-28 09:23:49 -0700

    Added highlight feature for courses sidebar

--- app/AdminPortal/StudentCourses/components/Sidebar.js
+++ app/AdminPortal/StudentCourses/components/Sidebar.js
@@ -1,28 +1,48 @@
 import Link from 'next/link';
-import styles from './component.module.css';
+import { useEffect, useState } from 'react';
+import styles from './components.module.css';
 
 const Sidebar = () => {
+  const [activePath, setActivePath] = useState('');
+
+  // Get the current path from the browser
+  useEffect(() => {
+    setActivePath(window.location.pathname);
+  }, []);
+
   return (
     <div className={styles.sidebar}>
       <h2>Student Courses</h2>
       <ul>
         <li>
-          <Link href="/AdminPortal/StudentCourses/grade11">
+          <Link
+            href="/AdminPortal/StudentCourses/grade11"
+            className={activePath === '/AdminPortal/StudentCourses/grade11' ? styles.active : ''}
+          >
             Senior High School Grade 11
           </Link>
         </li>
         <li>
-          <Link href="/AdminPortal/StudentCourses/grade12">
+          <Link
+            href="/AdminPortal/StudentCourses/grade12"
+            className={activePath === '/AdminPortal/StudentCourses/grade12' ? styles.active : ''}
+          >
             Senior High School Grade 12
           </Link>
         </li>
         <li>
-          <Link href="/AdminPortal/StudentCourses/BachelorWifery">
+          <Link
+            href="/AdminPortal/StudentCourses/BachelorWifery"
+            className={activePath === '/AdminPortal/StudentCourses/BachelorWifery' ? styles.active : ''}
+          >
             Bachelor of Science in Midwifery
           </Link>
         </li>
         <li>
-          <Link href="/AdminPortal/StudentCourses/BachelorEducation">
+          <Link
+            href="/AdminPortal/StudentCourses/BachelorEducation"
+            className={activePath === '/AdminPortal/StudentCourses/BachelorEducation' ? styles.active : ''}
+          >
             Bachelor of Technical-Vocational Teacher Education
           </Link>
         </li>
