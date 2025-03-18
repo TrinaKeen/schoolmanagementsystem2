5befd0a366abe22b268fb7b236f376c651bdecfd
Nabby <trishasalubreaquino@gmail.com>
2025-03-17 20:35:17 -0600

    putting this to my branch

--- Pages/api/admin/adminLogin.js
+++ Pages/api/admin/adminLogin.js
@@ -1,11 +1,6 @@
-import bcrypt from "bcryptjs";
 import { neon } from "@neondatabase/serverless";
-<<<<<<< Updated upstream
 import jwt from "jsonwebtoken"; // Make sure to install jsonwebtoken
-=======
-import jwt from "jsonwebtoken";
 import { serialize } from "cookie"; // To set the token cookie. Install cookie and cookie jsonwebtoken - Martin
->>>>>>> Stashed changes
 
 const sql = neon(process.env.DATABASE_URL);
 
@@ -47,8 +42,6 @@ export default async function handler(req, res) {
         }
       );
 
-<<<<<<< Updated upstream
-=======
       // Added by Martin
       res.setHeader(
         "Set-Cookie",
@@ -61,7 +54,6 @@ export default async function handler(req, res) {
         })
       );
 
->>>>>>> Stashed changes
       // Successful login
       res.status(200).json({
         message: "Login successful",
