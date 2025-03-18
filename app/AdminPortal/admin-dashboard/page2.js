3c9e870cc7ab9e32ed62fa50d09a748d60bb667a
~YUNA~ <151793031+TrinaKeen@users.noreply.github.com>
2025-03-18 00:37:27 -0700

    NewSidebar update
    Just added a new sidebar for the Dashboard that we can use for the entire pages

---
+++ app/AdminPortal/admin-dashboard/page2.js
@@ -0,0 +1,124 @@
+"use client";
+
+import Head from "next/head";
+import Image from "next/image";
+import Link from "next/link";
+import styles from "../components/HomePage.module.css";
+import teacherlogo from "/src/teachericon.png";
+import studentlogo from "/src/studentlogo.png";
+import courseslogo from "/src/courseslogo.png";
+import departmentslogo from "/src/departmentslogo.png";
+import studentfeeslogo from "/src/studentfeeslogo.png";
+import reportlogo from "/src/reportlogo.png";
+import AdminHeader from "../components/header";
+import Sidebar from "../components/Sidebar";
+
+export default function Home() {
+  return (
+    <div style={{ backgroundColor: 'white', height: '100vh', color: 'black'}}>
+    <Sidebar/>
+    <AdminHeader/>
+    <div className={styles.container}>
+      
+
+      
+
+      <main className={styles.main}>
+        <h2>Welcome to the Super Admin Account</h2>
+        <p>
+          Manage your school's daily activities, student records, teacher
+          assignments, and more.
+        </p>
+
+          <div className={styles.grid}>
+            <div className={styles.card}>
+              <Image
+                src={studentlogo}
+                alt="Student Logo"
+                width={200}
+                height={200}
+              />
+              <h3>Student Management</h3>
+              <p>Manage student records and progress.</p>
+              <Link href="../AdminPortal/StudentPage">
+                Go to Student Management
+              </Link>
+            </div>
+
+            <div className={styles.card}>
+              <Image
+                src={teacherlogo}
+                alt="Teacher Logo"
+                width={200}
+                height={200}
+              />
+              <h3>Teacher Management</h3>
+              <p>Manage teacher assignments and performance.</p>
+              <Link href="../AdminPortal/TeacherPage">
+                Go to Teacher Management{" "}
+              </Link>
+            </div>
+
+            <div className={styles.card}>
+              <Image
+                src={departmentslogo}
+                alt="Departments Logo"
+                width={200}
+                height={200}
+              />
+              <h3>School Departments</h3>
+              <p>Organize class schedules and attendance.</p>
+              <Link href="../AdminPortal/SchoolDepartment">
+                Go to Departments Management{" "}
+              </Link>
+            </div>
+          </div>
+
+          <div className={styles.grid}>
+            <div className={styles.card}>
+              <Image
+                src={courseslogo}
+                alt="Courses Logo"
+                width={200}
+                height={200}
+              />
+              <h3>Student Courses</h3>
+              <p>Manage student records and progress.</p>
+              <Link href="../AdminPortal/StudentCourses">
+                Go to Student Courses Management{" "}
+              </Link>
+            </div>
+
+            <div className={styles.card}>
+              <Image
+                src={studentfeeslogo}
+                alt="Student Fees Logo"
+                width={200}
+                height={200}
+              />
+              <h3>Student Fees</h3>
+              <p>Manage teacher assignments and performance.</p>
+              <Link href="../AdminPortal/StudentFees">
+                Go to Tuition Fees Management{" "}
+              </Link>
+            </div>
+
+          <div className={styles.card}>
+            <Image
+              src={reportlogo}
+              alt="Report Logo"
+              width={200}
+              height={200}
+            />
+            <h3>Reports</h3>
+            <p>Organize class schedules and attendance.</p>
+            <Link href="../AdminPortal/Reports">
+              Go to Reports Management{" "}
+            </Link>
+          </div>
+        </div>
+      </main>
+      </div>
+    </div>
+  );
+}
