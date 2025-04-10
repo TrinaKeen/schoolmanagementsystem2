6e29818f3d242457ef72130a11b046aabd03b0b7
Martin <martin.maquilan@edu.sait.ca>
2025-04-10 11:04:58 -0600

    Added courses page wip to sidebar

--- app/SchoolWebsite/ChatBox/app.tsx
+++
@@ -1,8 +0,0 @@
-import {MultiChatWindow} from 'react-chat-engine-advanced';
-function App() {
-    return <div>
-        <MultiChatWindow style={{height: "100vh"}}/>
-    </div>
-}
-
-export default App;
