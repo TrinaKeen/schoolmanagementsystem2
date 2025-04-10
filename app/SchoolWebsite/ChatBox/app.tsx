806e01d45ca3412727b93613605768a0a41c1ef3
emily-thieu56 <152931248+emily-thieu56@users.noreply.github.com>
2025-04-10 09:53:06 -0600

    Added chatbox folder and app (wip)

---
+++ app/SchoolWebsite/ChatBox/app.tsx
@@ -0,0 +1,8 @@
+import {MultiChatWindow} from 'react-chat-engine-advanced';
+function App() {
+    return <div>
+        <MultiChatWindow style={{height: "100vh"}}/>
+    </div>
+}
+
+export default App;
