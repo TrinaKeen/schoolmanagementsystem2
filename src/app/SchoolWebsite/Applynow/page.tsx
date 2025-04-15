e49f3395a97fce8ef90a13da6d91454a141ff3d9
~YUNA~ <151793031+TrinaKeen@users.noreply.github.com>
2025-04-15 09:00:51 -0600

    Update the links from website and menu in admin portal

--- src/app/SchoolWebsite/Applynow/page.tsx
+++ src/app/SchoolWebsite/Applynow/page.tsx
@@ -3,6 +3,11 @@
 import { useState } from 'react';
 import { useRouter } from 'next/navigation'; // Import from next/navigation
 import styles from '../../../components/styles/studentloginregister.module.css';
+import Head from "next/head";
+import Header from "../components/header";
+import Footer from "../components/footer";
+import Image from "next/image";
+import schoolPic from "/public/11.jpg"; // Adjust the path as necessary
 
 const SignupPage = () => {
   const router = useRouter();
@@ -64,6 +69,29 @@ const SignupPage = () => {
   
 
   return (
+    <div>
+      <Head>
+              <title>About Us - Evelyn E. Fabie College</title>
+              <meta
+                name="description"
+                content="Learn about the history, mission, and vision of Evelyn E. Fabie College, Inc."
+              />
+            </Head>
+            <Header />
+            <section className="relative h-[60vh] w-full font-sans">
+        <Image
+          src={schoolPic}
+          alt="About Page Cover"
+          fill
+          priority
+          className="object-cover"
+        />
+        <div className="absolute inset-0 bg-black/50 z-10" />
+        <div className="relative z-20 flex flex-col items-center justify-center h-full text-center px-4 text-white">
+          <h1 className="text-4xl md:text-5xl font-extrabold mb-4">Register Now!</h1>
+       </div>
+      </section>
+    
     <div className={styles.container}>
       <h1 className={styles.h1}>Sign Up</h1>
 
@@ -123,6 +151,8 @@ const SignupPage = () => {
           Close
         </button>
       </div>
+    </div>
+        <Footer />
     </div>
   );
 };
