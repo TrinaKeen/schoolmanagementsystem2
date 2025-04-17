faaa1cd23321a76eb67034ea63cbbce4c3631115
Martin Maquilan <141066621+MartinMaquilanS8@users.noreply.github.com>
2025-04-17 17:39:17 -0600

    drop down added in courses form

--- src/utils/fields/courseFields.ts
+++ src/utils/fields/courseFields.ts
@@ -1,15 +1,36 @@
-
-import { FieldConfig } from "./instructorFields";
-  
-  // Course fields array
-  // This array defines the structure of the form for adding a new course
-  const courseFields: FieldConfig[] = [
-    { name: 'courseCode', label: 'Course Code', required: true },
-    { name: 'courseName', label: 'Course Name', required: true },
-    { name: 'courseDescription', label: 'Description' },
-    { name: 'instructorId', label: 'Instructor ID', required: true },
-    { name: 'programId', label: 'Program ID' },
-  ];
+// courseFields.ts
+const courseFields = (
+  instructorOptions: { label: string; value: string }[],
+  programOptions: { label: string; value: string }[]
+) => [
+  {
+    name: "courseCode",
+    label: "Course Code",
+    required: true,
+  },
+  {
+    name: "courseName",
+    label: "Course Name",
+    required: true,
+  },
+  {
+    name: "courseDescription",
+    label: "Description",
+  },
+  {
+    name: "instructorId",
+    label: "Instructor",
+    type: "select",
+    required: true,
+    options: instructorOptions,
+  },
+  {
+    name: "programId",
+    label: "Program",
+    type: "select",
+    required: true,
+    options: programOptions,
+  },
+];
 
 export default courseFields;
-  
