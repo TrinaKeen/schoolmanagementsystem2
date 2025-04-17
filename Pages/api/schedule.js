be8b15eaf48281d9d37b1db8074676ec4283f451
emily-thieu56 <152931248+emily-thieu56@users.noreply.github.com>
2025-04-16 22:36:48 -0600

    Made payment and schedule (WIP)

---
+++ Pages/api/schedule.js
@@ -0,0 +1,39 @@
+import { PrismaClient } from "@prisma/client";
+import { parse } from "path";
+
+const prisma = new PrismaClient();
+
+export default async function handler(req,res) {
+    if (req.method === 'GET'){
+        try {
+            const schedules = await prisma.course.findMany({
+                include: {
+                    course: true,
+                    instructor: true,
+                },
+            });
+            res.status(200).json(schedules);
+        } catch (err) {
+            res.status(500).json({ error: 'Failed to fetch schedule'});
+        }
+    } else if (req.method === 'POST') {
+        const {  courseId, instructorId, startTime, endTime  } = req.body;
+        
+        try {
+            const newSchedule = await prisma.course.create({
+                data: {
+                    courseId: parseInt(courseId),
+                    instructorId: parseInt(instructorId),
+                    programId: programId ? parseInt(programId) : null,
+                    startTime,
+                    endTime
+                },
+            });
+            res.status(201).json(newSchedule);
+        } catch (err) {
+            res.status(500).json({ error: 'Failed to create schedule'});
+        }
+    } else {
+        res.status(405).json({ error: 'Method not allowed'});
+    }
+}
